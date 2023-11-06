214d0ace37864e9df0b4aa23ca536586d4cc5136
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-06 12:23:26 +0530

    Modal class created

---
+++ src/main/java/com/bhas/modal/Employee.java
@@ -0,0 +1,19 @@
+package com.bhas.modal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table
+public class Employee
+{
+    @Id
+    private int empId;
+}
