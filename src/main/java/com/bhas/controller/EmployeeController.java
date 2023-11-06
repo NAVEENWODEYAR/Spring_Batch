ebecabc5ed3e3a8b2a9cd7dec504497073d114c8
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-06 19:34:29 +0530

    <dependency> 			<groupId>org.springframework.boot</groupId> 			<artifactId>spring-boot-starter-batch</artifactId> 		</dependency>

--- src/main/java/com/bhas/controller/EmployeeController.java
+++ src/main/java/com/bhas/controller/EmployeeController.java
@@ -1,4 +1,4 @@
-package com.bhas.modal.controller;
+package com.bhas.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
