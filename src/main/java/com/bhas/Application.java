5ddfeb609116a2b6203a721331a7f3fa3b95ab4c
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-06 11:59:57 +0530

    Spring Batch

--- src/main/java/com/bhas/Application.java
+++ src/main/java/com/bhas/Application.java
@@ -10,6 +10,8 @@ public class Application
 	public static void main(String[] args)
 	{
 		SpringApplication.run(Application.class, args);
+		System.out.println("Spring Batch provides classes and APIs to read/write resources, transaction management, " +
+				"job processing statistics, job restart, and partitioning techniques to process high-volume data.");
 	}
 
 }
