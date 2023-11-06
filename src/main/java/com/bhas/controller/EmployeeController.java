7442bb9d81b772e2a42d20d6af5cb1dbe9d5d547
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-06 20:28:29 +0530

    public Job job(JobRepository jobRepository,Step step)

--- src/main/java/com/bhas/controller/EmployeeController.java
+++ src/main/java/com/bhas/controller/EmployeeController.java
@@ -1,6 +1,15 @@
 package com.bhas.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +17,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/employee")
 public class EmployeeController
 {
+
+    @Autowired
+    private JobLauncher jobLauncher;
+
+    @Autowired
+    private Job job;
+
     @GetMapping("/test/{st}")
     public String testPoint(String st)
     {
         return "Hello,"+st+" welcome to Employee App.";
+    }
+
+    @PostMapping("/doJob")
+    public void csvToDatabase() throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+        JobParameters parameters = new JobParametersBuilder()
+                .addLong("startAt",System.currentTimeMillis()).toJobParameters();
+
+        jobLauncher.run(job,parameters);
     }
 }
