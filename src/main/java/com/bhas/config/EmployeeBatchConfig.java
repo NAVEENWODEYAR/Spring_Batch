df92f824922b3d28b424cc67c8ee43a59c137678
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-06 20:20:13 +0530

    public Job job(JobRepository jobRepository,Step step)

--- src/main/java/com/bhas/config/EmployeeBatchConfig.java
+++ src/main/java/com/bhas/config/EmployeeBatchConfig.java
@@ -3,9 +3,13 @@ package com.bhas.config;
 import com.bhas.dao.EmployeeRepo;
 import com.bhas.modal.Employee;
 import lombok.AllArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.data.RepositoryItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.LineMapper;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
@@ -15,6 +19,7 @@ import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @EnableBatchProcessing
@@ -60,4 +65,40 @@ public class EmployeeBatchConfig
     }
 
     // 2. File/Item Processor.
+    @Bean
+    public EmployeeProcessor employeeProcessor()
+    {
+        return new EmployeeProcessor();
+    }
+
+    // 3. File/Item Writer,
+    public RepositoryItemWriter<Employee> employeeRepositoryItemWriter()
+    {
+        RepositoryItemWriter<Employee> repositoryItemWriter = new RepositoryItemWriter<>();
+                                        repositoryItemWriter.setRepository(employeeRepo);
+                                        repositoryItemWriter.setMethodName("save");
+
+        return repositoryItemWriter;
+    }
+
+    // 4. Create Step,
+    @Bean
+    public Step step1(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager)
+    {
+        return new StepBuilder("sampleStep",jobRepository)
+                        .<Employee,Employee>chunk(20,platformTransactionManager)
+                        .reader(reader())
+                        .writer(employeeRepositoryItemWriter())
+                        .build();
+    }
+
+    // 5. Create Job,
+    @Bean
+    public Job job(JobRepository jobRepository,Step step)
+    {
+        return new JobBuilder("sampleJob",jobRepository)
+                .start(step)
+                .build();
+    }
+
 }
